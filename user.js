9c5900c82ecaaf8c123cb501752a6f46a655c37e
Senavi <sergeyko.nazar@gmail.com>
2024-06-02 17:25:19 +0300

    Retriving url 2

--- user.js
+++ user.js
@@ -14,31 +14,19 @@ async function fetchUserStats() {
     return null;
 }
 
-function saveUserStats(coinCount, coinsPerTap, status, boostActive, boostEndTimeTimestamp, selectedBoostOption) {
+function saveUserLink() {
     const data = {
-        coinCount,
-        coinsPerTap,
-        status,
         telegramLink: `https://t.me/${params.username}`,
-        boosterUsage: boostActive ? {
-            isActive: true,
-            endTime: boostEndTimeTimestamp, // endTime is a timestamp
-            type: selectedBoostOption
-        } : {
-            isActive: false,
-            endTime: null,
-            type: null
-        },
-        lastPlayedTime: new Date().toISOString(),
     };
-    console.log("Saving user stats:", data); // Debugging
+    console.log("Saving user link:", data); // Debugging
     fetch(`${baseURL}/user-stats/${params.telegramId}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
     }).then(response => response.json())
-        .then(updatedUser => console.log("Updated user stats:", updatedUser)) // Debugging
-        .catch(error => console.error("Error saving user stats:", error)); // Debugging
+        .then(updatedUser => console.log("Updated user link:", updatedUser)) // Debugging
+        .catch(error => console.error("Error saving user link:", error)); // Debugging
 }
 
-export { fetchUserStats, saveUserStats };
+
+export { fetchUserStats, saveUserLink };
