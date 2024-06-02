39fae7b14203b866b4f926e02f3c0daf41aae39a
Senavi <sergeyko.nazar@gmail.com>
2024-06-02 18:20:55 +0300

    Latest changes 2

--- user.js
+++ user.js
@@ -2,7 +2,7 @@
 
 import { params } from './params.js';
 
-const baseURL = "https://sitcoincryptogame-ed48f5fed350.herokuapp.com/"; // Ensure this is correct and without trailing slash
+const baseURL = "https://sitcoincryptogame-ed48f5fed350.herokuapp.com"; // Ensure this is correct and without trailing slash
 
 async function fetchUserStats() {
     if (params.telegramId) {
@@ -14,19 +14,31 @@ async function fetchUserStats() {
     return null;
 }
 
-function saveUserStats() {
+function saveUserStats(coinCount, coinsPerTap, status, boostActive, boostEndTimeTimestamp, selectedBoostOption) {
     const data = {
+        coinCount,
+        coinsPerTap,
+        status,
         telegramLink: `https://t.me/${params.username}`,
+        boosterUsage: boostActive ? {
+            isActive: true,
+            endTime: boostEndTimeTimestamp, // endTime is a timestamp
+            type: selectedBoostOption
+        } : {
+            isActive: false,
+            endTime: null,
+            type: null
+        },
+        lastPlayedTime: new Date().toISOString(),
     };
-    console.log("Saving user link:", data); // Debugging
+    console.log("Saving user stats:", data); // Debugging
     fetch(`${baseURL}/user-stats/${params.telegramId}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
     }).then(response => response.json())
-        .then(updatedUser => console.log("Updated user link:", updatedUser)) // Debugging
-        .catch(error => console.error("Error saving user link:", error)); // Debugging
+        .then(updatedUser => console.log("Updated user stats:", updatedUser)) // Debugging
+        .catch(error => console.error("Error saving user stats:", error)); // Debugging
 }
-
 
 export { fetchUserStats, saveUserStats };
